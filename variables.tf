9d2b7b04948edd8acdd3fdb818b191077157fea9
Atharva Wathodkar <atharva3990@gmail.com>
2022-08-26 11:02:53 +0530

    account

--- variables.tf
+++ variables.tf
@@ -36,7 +36,7 @@ variable "cluster_name" {
 variable "cluster_service_account_name" {
   description = "The name of the custom service account used for the GKE cluster. This parameter is limited to a maximum of 28 characters."
   type        = string
-  default     = "example-cluster-sa"
+  default     = "example-cluster"
 }
 
 variable "cluster_service_account_description" {
